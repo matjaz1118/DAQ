34e71aa46103fc0290e87092b9ed87f69fecc080
Matjaz Tome <matjaz.tome@gmail.com>
2015-05-06 20:29:13 +0200

    Aqusition seems to work now. Steel needs some more debuging to confirm that. Inputs are steel single-ended.

--- USB_DAQ/src/ADC_core.c
+++ USB_DAQ/src/ADC_core.c
@@ -80,7 +80,6 @@ void ADC_init (void)
 	adc_set_resolution(ADC, ADC_MR_LOWRES_BITS_12);
 	adc_enable_interrupt(ADC, ADC_ISR_DRDY);
 	adc_configure_trigger(ADC, ADC_TRIG_SW, 0); // there is a bug in ASF driver for ADC, so free run mode has to be enabled manualy
-	adc_enable_freerun();
 	//adc_enable_channel(ADC, ADC_CHANNEL_4);
 	NVIC_SetPriority(ADC_IRQn, 1);
 	NVIC_EnableIRQ(ADC_IRQn);
@@ -90,29 +89,21 @@ void ADC_init (void)
 
 void aquisition_start (void)
 {
+
 	DAQSettingsPtr = get_current_DAQ_settings();
 	avgCounter = DAQSettingsPtr->avgCounter;
 	sampleCounter = DAQSettingsPtr->cycles;
 	DAQSettingsPtr->newData = FALSE;
 	adc_disable_all_channel(ADC);
-	adc_enable_channel(ADC, DAQSettingsPtr->sequence[sequencePosition]);
-	adc_start(ADC);
-	sequencePosition++;
-	if(!DAQSettingsPtr->sequence[sequencePosition]) {sequencePosition = 0; sampleCounter--;}
 	tc_write_rc(TC0, 0, DAQSettingsPtr->timerBase);
 	tc_start(TC0, 0);
 	//Setup adc and start the timer. Everithing else happens in TIMER ISR
+	
 }
 
 void aquisition_stop (void)
 {
 	tc_stop(TC0, 0);
-}
-
-void next_in_sequence (void)
-{
-	
-	
 }
 
 void ADC_Handler (void)
@@ -123,19 +114,19 @@ void ADC_Handler (void)
 	uint8_t printBuffer[20];
 	
 	status = adc_get_status(ADC);
-		accumulator += adc_get_latest_value(ADC);
-		avgCounter--;
-		if(avgCounter == 1) {adc_disable_freerun();}
-		if(!avgCounter)
-		{
-			//adc_stop(ADC);
-			adc_disable_freerun();
-			result = accumulator / DAQSettingsPtr->avgCounter;
-			accumulator = 0;
-			//todo: convert result to mV
-			charsPrinted = sprintf(printBuffer, "%u\n\r", result);
-			udi_cdc_write_buf(printBuffer, charsPrinted);
-		}
+	accumulator += adc_get_latest_value(ADC);
+	avgCounter--;
+	if(avgCounter == 1) {adc_disable_freerun(); adc_disable_all_channel(ADC);}
+	if(!avgCounter)
+	{
+		//adc_stop(ADC);
+		adc_disable_freerun();
+		result = accumulator  / DAQSettingsPtr->avgCounter; 
+		accumulator = 0;
+		//todo: convert result to mV
+		charsPrinted = sprintf(printBuffer, "%u\n\r", result);
+		udi_cdc_write_buf(printBuffer, charsPrinted);
+	}
 		
 		
 
@@ -148,36 +139,27 @@ void TC0_Handler (void)
 	volatile uint32_t ul_dummy;
 
 	// Clear status bit to acknowledge interrupt
-	ul_dummy = tc_get_status(TC0, 0);
-	
+	ul_dummy = tc_get_status(TC0, 0);	
 	pio_toggle_pin_group(PIOA, PIO_PA9);
 	if(sampleCounter)
 	{
+		avgCounter = DAQSettingsPtr->avgCounter;
 		adc_disable_all_channel(ADC);
-		if(DAQSettingsPtr->sequence[sequencePosition])
+		adc_enable_channel(ADC, DAQSettingsPtr->sequence[sequencePosition]);
+		adc_enable_freerun();
+		adc_start(ADC);
+		
+		sequencePosition++;
+		if(!DAQSettingsPtr->sequence[sequencePosition])
 		{
-			adc_enable_channel(ADC, DAQSettingsPtr->sequence[sequencePosition] - 1);
-			avgCounter = DAQSettingsPtr->avgCounter;
-			adc_enable_freerun();
-			adc_start(ADC);
-			sequencePosition++;
-			if(!DAQSettingsPtr->sequence[sequencePosition])
-			{
-				sequencePosition = 0;
-				sampleCounter--;
-				
-			}
-		}
-		else
-		{
-			sequencePosition = 0;
 			sampleCounter--;
+			sequencePosition = 0;
 		}
 	}
 	else
 	{
+		adc_disable_all_channel(ADC);
 		adc_disable_freerun();
 		tc_stop(TC0, 0);
-		//adc_stop(ADC);
 	}
 }
